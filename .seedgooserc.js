55b3a1ede22a14c3ee8589020538e4e33d200f7c
sud-Z <cieslanic2018@gmail.com>
2023-01-28 06:01:58 -0500

    Module 4

---
+++ .seedgooserc.js
@@ -0,0 +1,7 @@
+// .seedgoose.js
+module.exports = {
+    modelBaseDirectory: 'app_server/models', // model directory name
+    models : ["*.js", "!db.js"], // model matcher
+    data : "data", // data directory name
+    db : "mongodb://127.0.0.1:27017/travlr" // db connection url
+};
